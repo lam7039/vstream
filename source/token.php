9a053ccc43820bb03485992481833bbef00333bd
lamram <kelvin7039@gmail.com>
2020-05-24 01:42:04 +0200

    fixed token

--- source/token.php
+++ source/token.php
@@ -2,17 +2,21 @@
 
 namespace library;
 
+use function library\session\session_get;
+use function library\session\session_remove;
+use function library\session\session_set;
+
 class token {
     
     public static function generate() : string {
         $token = bin2hex(openssl_random_pseudo_bytes(64));
-        session::create(CONFIG('session/csrf_token'), $token);
+        session_set(CONFIG('session/csrf_token'), $token);
         return $token;
     }
 
     public static function check(string $token) : bool {
-        if (session::get(CONFIG('session/csrf_token')) === $token) {
-            session::delete(CONFIG('session/csrf_token'));
+        if (session_get(CONFIG('session/csrf_token')) === $token) {
+            session_remove(CONFIG('session/csrf_token'));
             return true;
         }
         return false;
