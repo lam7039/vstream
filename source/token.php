d6289b2cca79dee430f71025b6a84d2f72bfdfc6
lamram <kelvin7039@gmail.com>
2020-05-24 02:00:58 +0200

    changed token class into global functions

--- source/token.php
+++ source/token.php
@@ -6,19 +6,16 @@ use function library\session\session_get;
 use function library\session\session_remove;
 use function library\session\session_set;
 
-class token {
-    
-    public static function generate() : string {
-        $token = bin2hex(openssl_random_pseudo_bytes(64));
-        session_set(CONFIG('session/csrf_token'), $token);
-        return $token;
-    }
+function token_generate() : string {
+    $token = bin2hex(openssl_random_pseudo_bytes(64));
+    session_set(CONFIG('session/csrf_token'), $token);
+    return $token;
+}
 
-    public static function check(string $token) : bool {
-        if (session_get(CONFIG('session/csrf_token')) === $token) {
-            session_remove(CONFIG('session/csrf_token'));
-            return true;
-        }
-        return false;
+function token_check(string $token) : bool {
+    if (session_get(CONFIG('session/csrf_token')) === $token) {
+        session_remove(CONFIG('session/csrf_token'));
+        return true;
     }
+    return false;
 }
