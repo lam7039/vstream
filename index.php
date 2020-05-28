cb5d6e50dd6c0f2b58220a47a4750a32ab8e4481
lamram <kelvin7039@gmail.com>
2020-05-28 18:33:15 +0200

    added routes and .htaccess

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 set_include_path(__DIR__);
 require('core/init.php');
+require('routing.php');
 
 use library\file_buffer;
 use library\file_cache;
-use library\route;
 use library\template;
 
 $file_cache = new file_cache;
@@ -14,16 +14,12 @@ foreach ($html_files as $html_file) {
     $file_cache->cache_file(basename($html_file), new file_buffer('html/' . $html_file));
 }
 
-$route = new route;
-$route->set('/home', 'login.html');
-
 $templating = new template($file_cache);
-$templating->set_parameter('layout.html', 'page_title', 'Home');
+$templating->set_parameter('layout.html', 'page_title', 'vstream');
 $templating->set_parameter('layout.html', 'page_style', 'layout.css');
 $templating->set_parameter('login.html', 'test', 'login');
 
-if ($destination = $route->get('/home')) {
+$path = isset($_GET['request']) ? $_GET['request'] : 'browse';
+if ($destination = $route->get($path)) {
     echo $templating->render($destination);
-} else {
-    echo 'no page';
 }
---
+++ routing.php
@@ -0,0 +1,9 @@
+<?php
+
+use library\route;
+
+$route = new route;
+$route->set('browse', 'browse.html');
+$route->set('register', 'register.html');
+$route->set('login', 'login.html');
+$route->set('account', 'account.html');
