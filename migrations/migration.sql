f6a4cde1b1614062c7f126b23f970c70938ebb7d
Kelvin <kelvin7039@gmail.com>
2020-07-18 21:06:24 +0200

    changed timestamp to datetime

--- migrations/migration.sql
+++ migrations/migration.sql
@@ -12,7 +12,7 @@ create table if not exists `vstream`.`access` (
     `id` int unsigned not null auto_increment,
     `user_id` int unsigned not null,
     `ip_address` int(45) unsigned not null unique,
-    `expiry` timestamp not null,
+    `expiry` datetime not null,
     constraint `access_pk` primary key (`id`),
     constraint `users_fk` foreign key (`user_id`) references `vstream`.`users`(`id`)
 );
