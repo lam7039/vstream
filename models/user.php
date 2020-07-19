be2bda208f810f826dc188c221056756a813da75
Kelvin <kelvin7039@gmail.com>
2020-07-19 14:04:05 +0200

    added .vscode ignore

--- models/user.php
+++ models/user.php
@@ -12,8 +12,8 @@ class user extends model {
     }
 
     public function access_user_data(int $user_access_id, array $columns = ['*']) : ?object {
-        $select_str = $this->sql_columns($columns) . ', users_access.expiry';
-        $sql = "select $select_str from {$this->table} join users_access on {$this->table}.id = users_access.user_id where users_access.id = :user_access_id";
-		return $this->database->fetch($sql, ['user_access_id' => $user_access_id]) ?? null;
+         $select_str = $this->sql_columns($columns) . ', users_access.expiry';
+         $sql = "select $select_str from {$this->table} join users_access on {$this->table}.id = users_access.user_id where users_access.id = :user_access_id";
+        return $this->database->fetch($sql, ['user_access_id' => $user_access_id]) ?? null;
     }
 }
