358e1fafa3857341495ed4d01f9e6fc2bfbee5da
Kelvin <kelvin7039@gmail.com>
2020-09-10 01:21:03 +0200

    removed thumbnail encoding, added subtitle extract

--- source/transcoder.php
+++ source/transcoder.php
@@ -7,28 +7,33 @@ abstract class media_buffer {
     public string $source_path;
     public string $source_filename;
     public string $source_extension;
-    public string $output_extension;
-    public string $output_path;
 
-    public function __construct(string $source_path, string $folder = '') {
+    public string $output_path;
+    public string $output_filename;
+    public string $output_extension;
+    public string $output_path_full;
+
+    public function __construct(string $source_path, string $folder) {
         $this->source_path = $source_path;
         $this->source_filename = pathinfo($source_path, PATHINFO_FILENAME);
         $this->source_extension = pathinfo($source_path, PATHINFO_EXTENSION);
-        $this->output_path = 'public/media/';
-        $this->output_path .= $folder ? $folder . '/' : '';
-        $this->output_path .= "{$this->source_filename}.{$this->output_extension}";
+        $this->output_path = "public/media/{$folder}";
+        $this->output_filename = $this->source_filename; //TODO: create proper filename with regex
+        $this->output_path_full .= "{$this->output_path}/{$this->output_filename}.{$this->output_extension}";
     }
 }
 
 class video_buffer extends media_buffer {
     public int $duration;
     public string $duration_time;
+    public string $subtitle_path_full;
 
     public function __construct(string $source_path, int $duration) {
         $this->output_extension = 'mp4';
         $this->duration = $duration;
         $this->duration_time = date('H:i:s', $duration);
         parent::__construct($source_path, 'videos');
+        $this->subtitle_path_full = "{$this->output_path}/{$this->output_filename}.vtt";
     }
 }
 
@@ -48,6 +53,8 @@ class transcoder {
     //TODO: use temporary queue, detect if script is running, if so, add to temporary queue (queue in database?)
     //TODO: check if database queue has jobs, if not run the job, if a job is running, add to database queue, if the job is done processing, check in database queue if another job has to be run 
     //TODO: if queue has multiple jobs, run queries with execute_multiple
+    //TODO: separate selected subtitles and dynamically load them
+    //TODO: use cover instead of extracting thumbnail from video
 
     // -c:v stands for -codec:video
     // -crf stands for constant rate factor, it has a range of 0-51, 0 is lossless, 23 is default, 51 is worst, 18 is nearly visually lossless
@@ -58,9 +65,10 @@ class transcoder {
         'rate'      => '-crf 16',
     ];
 
-    private array $image_options = [
-        'format' => '-f image2',
-        'frames' => '-frames:v 1',
+    // -c:v stands for -codec:subtitle
+    private array $subtitle_options = [
+        'map'       => '-map 0:m:language:eng',
+        'codec'     => '-c:s copy',
     ];
 
     // skip waiting, but also removes stdio and stderr
@@ -70,20 +78,31 @@ class transcoder {
     public function option_set(string $key, string $option) : void {
         $this->options[$key] = $option;
     }
+
+    public function subtitle_option_set(string $key, string $option) : void {
+        $this->subtitle_options[$key] = $option;
+    }
     
     public function ffmpeg(media_buffer $buffer, bool $silent = true) : void {
         $silence = $silent ? $this->silence : '';
         
         // video encoding
         $options = implode(' ', $this->options);
-        $command = "ffmpeg -i {$buffer->source_path} $options {$buffer->output_path}" . $silence;
+        $command = "ffmpeg -i {$buffer->source_path} $options {$buffer->output_path_full}" . $silence;
         dd($command);
         shell_exec($command);
-        
-        // thumbnail encoding
-        $image_options = implode(' ', $this->image_options);
-        $image_command = "ffmpeg -ss 00:00:01.000 -i {$buffer->output_path} $image_options public/media/images/{$buffer->id}.jpg" . $silence;
-        dd($image_command);
-        shell_exec($image_command);
+
+        // subtitle extraction
+        // shell_exec("ffmpeg -i video -c copy -map 0:s -f null - -v 0 -hide_banner && echo $? || echo $?"); // check if any subtitle exists
+        if ($buffer->output_extension === 'mp4') {
+            $this->subtitles($buffer, $silence);
+        }
+    }
+
+    public function subtitles(video_buffer $buffer, string $silence) : void {
+        $subtitle_options = implode(' ', $this->subtitle_options);
+        $subtitle_command  = "ffmpeg -i {$buffer->source_path} $subtitle_options {$buffer->subtitle_path_full}" . $silence;
+        dd($subtitle_command);
+        shell_exec($subtitle_command);
     }
 }
