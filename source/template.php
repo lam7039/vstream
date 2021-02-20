7f965719a9e2b479c2ddd87d0f96810be6972b53
Kelvin <kelvin7039@gmail.com>
2021-02-21 00:23:37 +0100

    removed legacy

--- source/template.php
+++ source/template.php
@@ -21,53 +21,6 @@ class token_node {
     ) {}
 }
 
-//Legacy
-class _template {
-    private file_buffer $layout;
-
-    public function __construct(array $parameters = [], string $template_path = 'public/templates/layout.html') {
-        $this->layout = new file_buffer($template_path);
-
-        if ($parameters) {
-            $this->layout = $this->bind_parameters($this->layout, $parameters);
-        }
-    }
-    
-    public function bind_parameter(file_buffer $buffer, string $key, string $value) : file_buffer {
-        if (!str_contains($buffer->body, "{{{$key}}}")) {
-            LOG_INFO("Parameter {{{$key}}} does not exist");
-            return $buffer;
-        }
-
-        $buffer->body = str_replace("{{{$key}}}", $value, $buffer->body);
-        return $buffer;
-    }
-
-    public function bind_parameters(file_buffer $buffer, array $parameters) : file_buffer {
-        foreach ($parameters as $key => $value) {
-            $buffer = $this->bind_parameter($buffer, $key, $value);
-        }
-
-        return $buffer;
-    }
-
-    public function render(file_buffer $buffer, bool $cache = false) : string {
-        if ($cache) {
-            $file = 'tmp/cache/' . md5($buffer->path);
-            if (file_exists($file) && (filemtime($file) + 3600) > time()) {
-                return file_get_contents($file);
-            }
-        }
-        
-        $buffer->body = str_replace('{{yield}}', $buffer->body, $this->layout->body);
-        if ($cache) {
-            file_put_contents($file, $buffer->body);
-        }
-        
-        return $buffer->body;
-    }
-}
-
 class template {
     private file_buffer $layout;
     private array $parameters = [];
