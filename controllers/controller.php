f52d7cb432c3a41f886ae3e5eddbf1956bc88826
lamram <kelvin7039@gmail.com>
2024-04-12 16:24:42 +0200

    add instead of merge

--- controllers/controller.php
+++ controllers/controller.php
@@ -9,15 +9,15 @@ abstract class controller {
 }
 
 abstract class page_controller extends controller {
+    protected template $templating;
     protected $parameters = [
         'page_favicon' => 'favicon-32x32.png',
         'page_style' => 'layout.css',
         'page_script' => 'script.js',
     ];
-    protected template $templating;
 
     public function __construct (array $parameters = []) {
-        $this->parameters = array_merge($this->parameters, $parameters);
+        array_push($this->parameters, ...$parameters);
         if (isset($parameters['url_page'])) {
             $this->parameters['page_title'] = env('PROJECT_NAME') . ($parameters['url_page'] ? " | " . $parameters['url_page'] : '');
         }
