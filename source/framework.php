abeccfe2b25e828f9a533513201fe4a84d33fc17
lamram <kelvin7039@gmail.com>
2024-04-13 18:24:57 +0200

    rm useless check

--- source/framework.php
+++ source/framework.php
@@ -4,16 +4,12 @@ namespace source;
 
 class Framework {
     public function __construct(
-        public container $container,
-        public request $request,
-        public router $router
+        private Container $container,
+        private Request $request,
+        private router $router
     ) {}
 
     public function run() {
-        $response = $this->router->resolve($this->container);
-        if ($response instanceof page_controller) {
-            $response = $response->index();
-        }
-        echo $response;
+        echo $this->router->resolve($this->container);
     }
 }
