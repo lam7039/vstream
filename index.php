d74152c9b496bb405b2f0987ad7a11a8ffd34463
lamram <kelvin7039@gmail.com>
2024-04-13 18:19:23 +0200

    cleaned up fixed index comments

--- index.php
+++ index.php
@@ -8,43 +8,12 @@ set_include_path(__DIR__);
 require('core/init.php');
 require('routing.php');
 
-/*
+// use function source\session_clear_temp;
+// use function source\session_once;
 
-use controllers\browse;
-use controllers\account;
-use controllers\authentication;
-
-use function source\session_clear_temp;
-use function source\session_once;
-
-$response = $router->response();
-
-if (isset($response['error'])) {
-    session_once('error', $response['error']);
-}
-
-if (!$response || (is_string($response) && !is_file($response)) || isset($response['path'])) {
-    redirect($response['path'] ?? env('HOMEPAGE'));
-}
-
-$output = match($identifier['url_page']) {
-    'login' => new authentication($identifier),
-    'register' => new authentication($identifier),
-    'account' => new account($identifier),
-    'browse' => new browse($identifier),
-    default => new browse($identifier)
-};
-
-/*
-TODO: rearrange the classes like this
-
-$container = new container;
-$router = new router($container);
-
-$router->registerRoutesFromControllerAttributes([
-    authentication::class
-]);
-*/
+// if (isset($response['error'])) {
+//     session_once('error', $response['error']);
+// }
 
 (new Framework(
     $container,
