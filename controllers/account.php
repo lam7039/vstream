b11f250d3fff8150c7465ed989f01875e2485a62
lamram <kelvin7039@gmail.com>
2024-12-27 00:01:03 +0100

    small refactor of account page controller

--- controllers/account.php
+++ controllers/account.php
@@ -11,14 +11,14 @@ use function source\session_get;
 use function source\session_isset;
 
 class account extends page_controller {
-    private ?user $user = null; 
+    private user $user; 
 
     public function __construct(Template $templating, Request $request) {
         parent::__construct($templating, $request);
-
+        
         $this->user = new user()->find(['id' => session_get(env('SESSION_AUTH'))]);
 
-        $parameters = [
+        $templating->bind_parameters([
             'username' => $this->user->username,
             'ip' => $this->user ? long2ip($this->user->ip_address) : '',
             'testfor' => [
@@ -26,8 +26,6 @@ class account extends page_controller {
                 'second',
                 'third',
             ]
-        ];
-
-        $templating->bind_parameters($parameters);
+        ]);
     }
 }
