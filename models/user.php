24e50e66d0d65ccb88d1dbd6d099098b9c6f11b7
lamram <kelvin7039@gmail.com>
2024-12-26 23:45:16 +0100

    private set public get for user model

--- models/user.php
+++ models/user.php
@@ -4,16 +4,8 @@ namespace models;
 
 class user extends model {
     protected string $table = 'users';
-    public int $id;
-    public string $username;
-    public string $password;
-    public int $ip_address;
-
-    // private(set) int $id;
-    // private(set) string $username;
-
-    // public function __construct($id, $username)
-    // {
-    //     parent::__construct()
-    // }
+    private(set) int $id;
+    private(set) string $username;
+    private(set) string $password;
+    private(set) int $ip_address;
 }
